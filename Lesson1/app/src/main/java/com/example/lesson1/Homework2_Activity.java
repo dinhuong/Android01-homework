396f2cdfd142f23ebf754f8190e5e9982e7f469e
dinhuong <rdinh522@gmail.com>
2020-03-12 00:29:51 +0700

    first commit

---
+++ Lesson1/app/src/main/java/com/example/lesson1/Homework2_Activity.java
@@ -0,0 +1,14 @@
+package com.example.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homework2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework2_);
+    }
+}
